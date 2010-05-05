4d84babc42d032c4f877b8e9bac0f211fe844d9e
Dmitry Shmidt <dimitrysh@google.com>
2010-05-05 12:57:27 -0700

    e2fsprogs: Add option to choose USE_E2FSPROGS
    Change-Id: I63eee7a6cd84c383386260d63ac91237c801b260
    Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

--- Android.mk
+++ Android.mk
@@ -1,5 +1,9 @@
 ifneq ($(TARGET_SIMULATOR),true)
 ifeq ($(TARGET_ARCH),x86)
 include $(call all-subdir-makefiles)
+else
+ifeq ($(USE_E2FSPROGS),true)
+include $(call all-subdir-makefiles)
+endif
 endif
 endif
