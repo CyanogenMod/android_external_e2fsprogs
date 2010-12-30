26c7f71ba042a41ebe3e6d4118fc7350311ca45d
Giulio Cervera <giulio.cervera@gmail.com>
2010-12-30 12:47:24 +0100

    build e2label
    Change-Id: I089f5b0a6a294f15d9a7eac5f8cb6e938b823b00

--- misc/Android.mk
+++ misc/Android.mk
@@ -159,3 +159,48 @@ LOCAL_SYSTEM_SHARED_LIBRARIES := \
 
 include $(BUILD_EXECUTABLE)
 
+#########################################################################
+# Build e2label
+#
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	e2label.c
+
+LOCAL_C_INCLUDES := \
+	external/e2fsprogs/lib
+
+LOCAL_CFLAGS := -Os -g -W -Wall \
+	-DHAVE_UNISTD_H \
+	-DHAVE_ERRNO_H \
+	-DHAVE_NETINET_IN_H \
+	-DHAVE_SYS_IOCTL_H \
+	-DHAVE_SYS_MMAN_H \
+	-DHAVE_SYS_MOUNT_H \
+	-DHAVE_SYS_PRCTL_H \
+	-DHAVE_SYS_RESOURCE_H \
+	-DHAVE_SYS_SELECT_H \
+	-DHAVE_SYS_STAT_H \
+	-DHAVE_SYS_TYPES_H \
+	-DHAVE_STDLIB_H \
+	-DHAVE_STRCASECMP \
+	-DHAVE_STRDUP \
+	-DHAVE_MMAP \
+	-DHAVE_UTIME_H \
+	-DHAVE_GETPAGESIZE \
+	-DHAVE_LSEEK64 \
+	-DHAVE_LSEEK64_PROTOTYPE \
+	-DHAVE_EXT2_IOCTLS \
+	-DHAVE_LINUX_FD_H \
+	-DHAVE_TYPE_SSIZE_T \
+	-DHAVE_GETOPT_H
+
+LOCAL_MODULE := e2label
+LOCAL_MODULE_TAGS := eng
+
+LOCAL_SYSTEM_SHARED_LIBRARIES := \
+	libext2fs \
+	libext2_com_err \
+	libc
+
+include $(BUILD_EXECUTABLE)
