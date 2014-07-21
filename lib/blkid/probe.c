973e06fbc84f4c7bcebfc1d78c1f4ae7f2d8dcef
invisiblek <dan.pasanen@gmail.com>
2014-07-20 22:45:42 -0500

    blkid: add f2fs support
    Change-Id: Ib6eeb904cf4825c8341fb0656a7130c724c86cd3

--- lib/blkid/probe.c
+++ lib/blkid/probe.c
@@ -1492,6 +1492,7 @@ static struct blkid_magic type_array[] = {
   { "lvm2pv",	 1,  0x018,  8, "LVM2 001",		probe_lvm2 },
   { "lvm2pv",	 1,  0x218,  8, "LVM2 001",		probe_lvm2 },
   { "btrfs",	 64,  0x40,  8, "_BHRfS_M",		probe_btrfs },
+  { "f2fs",	 1,	 0,  8, "F2F52010",		0},
   {   NULL,	 0,	 0,  0, NULL,			NULL }
 };
 
