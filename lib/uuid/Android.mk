ce5b6554b9d44e081e7c1f80d430188c23d86835
Jeff Sharkey <jsharkey@android.com>
2015-02-20 14:45:03 -0800

    Export include details from libuuid.
    Change-Id: I682e38c9710dcd91b20b8cba3bb2e32b2dbc61c4

--- lib/uuid/Android.mk
+++ lib/uuid/Android.mk
@@ -49,6 +49,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(libext2_uuid_src_files)
 LOCAL_C_INCLUDES := $(libext2_uuid_c_includes)
 LOCAL_CFLAGS := $(libext2_uuid_cflags)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_SYSTEM_SHARED_LIBRARIES := $(libext2_uuid_system_shared_libraries)
 LOCAL_MODULE := libext2_uuid
 LOCAL_MODULE_TAGS := optional
@@ -60,6 +61,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(libext2_uuid_src_files)
 LOCAL_C_INCLUDES := $(libext2_uuid_c_includes)
 LOCAL_CFLAGS := $(libext2_uuid_cflags)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_MODULE := libext2_uuid_host
 LOCAL_MODULE_TAGS := optional
 
@@ -71,6 +73,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(libext2_uuid_src_files)
 LOCAL_C_INCLUDES := $(libext2_uuid_c_includes)
 LOCAL_CFLAGS := $(libext2_uuid_cflags)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_STATIC_LIBRARIES := libc
 LOCAL_MODULE := libext2_uuid_static
 LOCAL_MODULE_TAGS := optional
@@ -83,6 +86,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(libext2_uuid_src_files)
 LOCAL_C_INCLUDES := $(libext2_uuid_c_includes)
 LOCAL_CFLAGS := $(libext2_uuid_cflags)
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)
 LOCAL_MODULE := libext2_uuid_host
 LOCAL_MODULE_TAGS := optional
 
